ba6c820c7345024e3aa54930dd159c41f2a90c6e
HYEOK9 <leejaehyuck9@gmail.com>
2021-11-10 17:45:45 +0900

    resultpage

--- frontend/src/views/result.vue
+++ frontend/src/views/result.vue
@@ -1,5 +1,24 @@
 <template>
-    <v-app></v-app>
+    <v-app
+        ><v-row align="center" justify="center"
+            ><v-spacer></v-spacer>
+            <v-col align="center" cols="auto"
+                ><br /><br /><br /><br />
+                <h1 style="color: white">
+                    이 주변엔 ___개의 치킨집이 영업중이고
+                </h1>
+                <br /><br />
+                <v-divider></v-divider>
+                <h2 style="color: white">치킨지수는 ___점 입니다!</h2>
+                <br />
+                <span style="color: white">
+                    (치킨지수=유동인구/치킨집수)
+                </span></v-col
+            ><v-spacer></v-spacer></v-row
+        ><v-row justify="center"
+            ><v-btn to="/search">다시하기</v-btn></v-row
+        ></v-app
+    >
 </template>
 <script>
 export default {
@@ -8,8 +27,6 @@ export default {
 </script>
 <style scoped>
 #app {
-    height: 100vh;
-    background: url('../assets/backimage.jpg') no-repeat center center fixed;
-    background-size: cover;
+    background-color: black;
 }
 </style>
--- frontend/src/views/search.vue
+++ frontend/src/views/search.vue
@@ -80,9 +80,10 @@ export default {
         initMap() {
             var container = document.getElementById('map');
             var options = {
-                center: new kakao.maps.LatLng(33.450701, 126.570667),
+                center: new kakao.maps.LatLng(37.5666805, 126.9784147),
                 level: 5,
             };
+
             var map = new kakao.maps.Map(container, options);
             map.setMapTypeId(kakao.maps.MapTypeId.LOADMAP);
         },
@@ -96,7 +97,8 @@ export default {
     background-size: cover;
 }
 #map {
-    width: 50vw;
-    height: 70vh;
+    width: 40vw;
+    height: 80vh;
+    border-radius: 3rem;
 }
 </style>
